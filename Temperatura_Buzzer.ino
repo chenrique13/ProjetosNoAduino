ef14ccfd45e0a1b8b2d396fa40e67b10f9bab895
Carlos Pereira <56885446+chenrique13@users.noreply.github.com>
2019-11-25 14:17:39 -0300

    Add files via upload

---
+++ Temperatura_Buzzer.ino
@@ -0,0 +1,26 @@
+const int LM35 = 0;
+float temperatura = 0;
+int ADClido = 0;
+const int Buzzer = 12;
+void setup(){
+Serial.begin(9600);
+analogReference(INTERNAL);
+pinMode(Buzzer, OUTPUT);
+}
+void loop(){
+ADClido = analogRead(LM35);
+temperatura = ADClido * 0.1075268817204301;
+Serial.print("Temperatura = ");
+Serial.print(temperatura);
+Serial.println(" *C");
+delay(2000);
+if(temperatura > 32){ 
+delay(1000);
+if(temperatura > 32){
+tone(Buzzer, 2000);
+}
+}
+else{
+noTone(Buzzer);
+}
+}
