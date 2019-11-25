bf2ca4f81fa9de25f0bedf06658db86e84101ed2
Carlos Pereira <56885446+chenrique13@users.noreply.github.com>
2019-11-25 14:16:51 -0300

    Add files via upload

---
+++ Poste.ino
@@ -0,0 +1,25 @@
+const int LDR = 0;
+const int Led = 3;
+int Intensidade = 0;
+
+void setup() {
+ Serial.begin(9600); 
+ pinMode(Led, OUTPUT);
+}
+void loop() {
+ Intensidade = analogRead(LDR);
+ Serial.print("Intensidade do Sensor LDR = ");
+ Serial.print(Intensidade);
+ Serial.println(" cd");
+ delay(2000);
+ 
+ if (Intensidade < 100){
+ digitalWrite(Led, HIGH);
+ }
+ else{
+ digitalWrite(Led, LOW);
+
+ }
+
+
+} 
