e8af69f475af5d851f2c2fef401fa8fdd2851da1
Carlos Pereira <56885446+chenrique13@users.noreply.github.com>
2019-11-25 14:16:42 -0300

    Add files via upload

---
+++ Pisca_led_RGB.ino
@@ -0,0 +1,22 @@
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+
+}
+
+void loop() {
+ digitalWrite(3,HIGH);
+ delay (50);
+ digitalWrite(3,LOW);
+
+ digitalWrite(4,HIGH);
+ delay (50);
+ digitalWrite(4,LOW);
+ 
+ digitalWrite(5,HIGH);
+ delay (50);
+ digitalWrite(5,LOW);
+
+
+}
